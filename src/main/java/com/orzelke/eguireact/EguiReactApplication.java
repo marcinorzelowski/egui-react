00c7b5026f164e6fdbe838e372df4568b50f429d
Marcin Orzełowski <marcin.hubert.orzelowski@gmail.com>
2020-05-24 10:44:26 +0200

    first commit

---
+++ src/main/java/com/orzelke/eguireact/EguiReactApplication.java
@@ -0,0 +1,13 @@
+package com.orzelke.eguireact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EguiReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EguiReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orzelke/eguireact/EguiReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orzelke.eguireact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EguiReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
